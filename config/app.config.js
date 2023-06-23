11b2e56e908c066c3613881c76ccc9f3477503ac
Ndong Henry Ndang <ndonghenryndang@gmail.com>
2023-06-23 15:52:47 +0100

    backend complete

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://ndonghenryndang:Oxwr7aS1r1aCUkEJ@myreactbackend.dhjwjk2.mongodb.net/myreactbackend?retryWrites=true&w=majority'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
